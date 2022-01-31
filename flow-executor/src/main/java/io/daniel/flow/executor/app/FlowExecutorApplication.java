283e1aa33cc5f3d010250c9e24a4c85573f92259
neason-cn <cqwjsl123@126.com>
2022-01-31 22:51:08 +0800

    init portal and executor application

---
+++ flow-executor/src/main/java/io/daniel/flow/executor/app/FlowExecutorApplication.java
@@ -0,0 +1,17 @@
+package io.daniel.flow.executor.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author neason-cn
+ * @date 2022/1/31
+ */
+@SpringBootApplication
+public class FlowExecutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowExecutorApplication.class, args);
+    }
+
+}
---
+++ flow-portal/src/main/java/io/daniel/flow/portal/app/FlowPortalApplication.java
@@ -0,0 +1,17 @@
+package io.daniel.flow.portal.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author neason-cn
+ * @date 2022/1/31
+ */
+@SpringBootApplication
+public class FlowPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowPortalApplication.class, args);
+    }
+
+}
