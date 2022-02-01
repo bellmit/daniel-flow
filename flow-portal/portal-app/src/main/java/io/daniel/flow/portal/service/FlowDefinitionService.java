7e51e72d9be23c5171b4ea2d231081e27d6261fb
neason-cn <cqwjsl123@126.com>
2022-02-01 19:11:59 +0800

    flow service def

--- flow-portal/portal-app/src/main/java/io/daniel/flow/portal/service/FlowDefinitionService.java
+++ flow-portal/portal-app/src/main/java/io/daniel/flow/portal/service/FlowDefinitionService.java
@@ -1,5 +1,7 @@
 package io.daniel.flow.portal.service;
 
+import io.daniel.flow.portal.domain.aggregate.FlowDefinition;
+
 /**
  * 流程定义/模板相关的Service
  *
@@ -7,4 +9,30 @@ package io.daniel.flow.portal.service;
  * @date 2022/2/1
  */
 public interface FlowDefinitionService {
+
+    /**
+     * 保存流程模板定义
+     */
+    void save(FlowDefinition definition);
+
+    /**
+     * enable开关
+     */
+    void exchange(String definitionId, boolean enable);
+
+    /**
+     * 通过ID获取流程模板定义
+     */
+    FlowDefinition getById(String definitionId);
+
+    /**
+     * 通过code获取流程模板最新版本的定义
+     */
+    FlowDefinition getByCode(String definitionCode);
+
+    /**
+     * 通过code和指定的version获取流程模板定义
+     */
+    FlowDefinition getByCodeAndVersion(String definitionCode, String version);
+
 }
--- flow-portal/portal-app/src/main/java/io/daniel/flow/portal/service/FlowInstanceService.java
+++ flow-portal/portal-app/src/main/java/io/daniel/flow/portal/service/FlowInstanceService.java
@@ -1,5 +1,10 @@
 package io.daniel.flow.portal.service;
 
+import io.daniel.flow.connector.domain.TaskExecuteResult;
+import io.daniel.flow.portal.domain.aggregate.FlowInstance;
+
+import java.util.Map;
+
 /**
  * 流程实例的相关Service
  *
@@ -7,4 +12,40 @@ package io.daniel.flow.portal.service;
  * @date 2022/2/1
  */
 public interface FlowInstanceService {
+
+    /**
+     * 根据流程模板定义的ID创建实例
+     */
+    FlowInstance createWithId(String definitionId);
+
+    /**
+     * 根据流程模板定义code的最新版本创建实例
+     */
+    FlowInstance createWithCode(String definitionCode);
+
+    /**
+     * 根据流程模板定义code和指定的版本创建实例
+     */
+    FlowInstance createWithCodeAndVersion(String definitionCode, String version);
+
+    /**
+     * 重新唤醒一个流程实例，并推进
+     */
+    void resume(String instanceId, String nodeCode, Map<String, String> params);
+
+    /**
+     * 处理Executor返回的执行结果
+     */
+    void handleCallBack(TaskExecuteResult callback);
+
+    /**
+     * 跳过一个节点
+     */
+    void skip(String instanceId, String nodeCode);
+
+    /**
+     * 取消一个流程实例
+     */
+    void cancel(String instanceId);
+
 }
